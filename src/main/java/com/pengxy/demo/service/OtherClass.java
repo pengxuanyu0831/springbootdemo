39cec32887dca902fa8f4bcf095dd1dec5a6f83f
pengxuanyu <pengxuanyus@gmail.com>
2020-11-18 16:36:31 +0800

    死锁了。。还要改

--- src/main/java/com/pengxy/demo/service/OtherClass.java
+++ src/main/java/com/pengxy/demo/service/OtherClass.java
@@ -16,26 +16,58 @@ import java.util.concurrent.TimeUnit;
 import static java.util.Arrays.asList;
 
 public class OtherClass {
+    public static final List<String> list = asList("A","1","B","2","C","3","D","4");
     public static void main(String[] args) {
-        new Thread(){
+        // A 线程打印字母
+        Thread A = new Thread(new Runnable() {
             @Override
-            public void run(){
-                for(int i = 1 ;i<4;i++){
-                    System.out.println(i);
-
+            public void run() {
+                synchronized (list){
+                    for(int i = 0;i <list.size();i+=2){
+                        System.out.println("线程 " + Thread.currentThread().getName() +" 的打印结果："+ i);
+                        list.notify();
+                        try{
+                            list.wait();
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
                 }
-
-            };
-        }.start();
-
+            }
+        },"A");
+//        {
+//            @Override
+//            public void run(){
+//                synchronized (list){
+//                    for(int i = 0;i <list.size();i+=2){
+//
+//                        try{
+//                            list.wait();
+//                        }catch (InterruptedException e){
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }
+//
+//            };
+//        }.start();
+    // B 线程打印数字
         new Thread(){
             @Override
             public void  run(){
                 //List list = Lists.newArrary()
-                List<String> list = asList("A","B","C","D");
-                for(int i = 0 ;i<list.size();i++){
-                    System.out.println(list.get(i));
+                synchronized (list){
+                    for(int i = 1 ;i< list.size()+1;i+=3){
+                        System.out.println("线程 " + Thread.currentThread().getName() +" 的打印结果："+ i);
+                        list.notify();
+                    }try{
+                        list.wait();
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
                 }
+
+
             };
         }.start();
     }
