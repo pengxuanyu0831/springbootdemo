a767c22b51ce145c1af9dd498db9f3cea1e75b64
pengxy <pengxuanyus@gmail.com>
2020-11-22 21:29:30 +0800

    redis 分布式锁demo

---
+++ src/main/java/com/pengxy/demo/redis/RedisLockDemo.java
@@ -0,0 +1,47 @@
+package com.pengxy.demo.redis;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @program demo
+ * @description: redis demo1,扣减库存
+ * @author: pengxuanyu
+ * @create: 2020/11/22 14:00
+ */
+public class RedisLockDemo {
+    private static  Integer inventory = 11;
+    private static  final int NUM = 10;
+    public static LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue<>();
+    static ReentrantLock rLock = new ReentrantLock();
+
+    public static void main(String[] args) throws InterruptedException {
+        // TimeUnit unit;
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(inventory, inventory, 10L, TimeUnit.SECONDS, linkedBlockingQueue);
+        final CountDownLatch latch =new CountDownLatch(NUM);
+        long startTime = System.currentTimeMillis();
+        for(int i = 0 ; i <= NUM ;i++){
+            pool.execute(new Runnable(){
+                @Override
+                public void run() {
+                    rLock.lock();
+                    inventory--;
+                    System.out.println("线程 "+ Thread.currentThread().getName());
+                    rLock.unlock();
+                    latch.countDown();
+                }
+            });
+            pool.shutdown();
+            try{
+                latch.await();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println("共计耗时"+(endTime - startTime)+"，此时库存数为"+ inventory);
+        }
+    }
+}
