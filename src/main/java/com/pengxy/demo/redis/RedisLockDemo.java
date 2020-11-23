640d10c9ee764417f175397fb5b4be8d3a85c212
pengxy <pengxuanyus@gmail.com>
2020-11-23 22:05:24 +0800

    优化了一下线程池

--- src/main/java/com/pengxy/demo/redis/RedisLockDemo.java
+++ src/main/java/com/pengxy/demo/redis/RedisLockDemo.java
@@ -13,35 +13,36 @@ import java.util.concurrent.locks.ReentrantLock;
  * @create: 2020/11/22 14:00
  */
 public class RedisLockDemo {
-    private static  Integer inventory = 11;
-    private static  final int NUM = 10;
-    public static LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue<>();
+    private static  Integer inventory = 1001;
+    private static int NUM = 1000;
+    private static LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
     static ReentrantLock rLock = new ReentrantLock();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         // TimeUnit unit;
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(inventory, inventory, 10L, TimeUnit.SECONDS, linkedBlockingQueue);
-        final CountDownLatch latch =new CountDownLatch(NUM);
+        ThreadPoolExecutor pool =
+                new ThreadPoolExecutor(inventory, inventory, 10L, TimeUnit.SECONDS, linkedBlockingQueue);
+        CountDownLatch latch =new CountDownLatch(NUM);
         long startTime = System.currentTimeMillis();
-        for(int i = 0 ; i <= NUM ;i++){
-            pool.execute(new Runnable(){
-                @Override
+        for(int i = 0 ; i <= NUM ;i++) {
+            pool.execute(new Runnable() {
+                //  @Override
                 public void run() {
-                    rLock.lock();
+                    //rLock.lock();
                     inventory--;
-                    System.out.println("线程 "+ Thread.currentThread().getName());
-                    rLock.unlock();
+                    System.out.println("线程 " + Thread.currentThread().getName());
+                    //rLock.unlock();
                     latch.countDown();
                 }
             });
-            pool.shutdown();
-            try{
-                latch.await();
-            }catch (InterruptedException e){
-                e.printStackTrace();
-            }
-            long endTime = System.currentTimeMillis();
-            System.out.println("共计耗时"+(endTime - startTime)+"，此时库存数为"+ inventory);
         }
+        pool.shutdown();
+        try{
+            latch.await();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("共计耗时"+(endTime - startTime)+"，此时库存数为"+ inventory);
     }
 }
