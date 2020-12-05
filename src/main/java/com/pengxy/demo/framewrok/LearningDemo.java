193975d4e8a0049facefe3d7f7cb9947e902fc10
pengxy <pengxuanyus@gmail.com>
2020-12-05 19:02:04 +0800

    委托者设计模式 & BeanDefinition

--- src/main/java/com/pengxy/demo/framewrok/LearningDemo.java
+++ src/main/java/com/pengxy/demo/framewrok/LearningDemo.java
@@ -1,6 +1,13 @@
 package com.pengxy.demo.framewrok;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @program demo
@@ -9,5 +16,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @create: 2020/12/02 21:53
  */
 public class LearningDemo {
-    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    BeanFactory bf = new XmlBeanFactory(new ClassPathResource( "spring.xml"));
+    Resource resource = new ClassPathResource("spring.xml");
+    InputStream inputStream = resource.getInputStream();
+
+    public LearningDemo() throws IOException {
+    }
 }
