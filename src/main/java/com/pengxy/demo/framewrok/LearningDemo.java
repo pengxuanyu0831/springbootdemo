55b20e60e4817b98fda8a6cdc3dbe3863491f2c0
pengxy <pengxuanyus@gmail.com>
2020-12-06 21:10:44 +0800

    spring 中递归扫描basepackage的实现逻辑

--- src/main/java/com/pengxy/demo/framewrok/LearningDemo.java
+++ src/main/java/com/pengxy/demo/framewrok/LearningDemo.java
@@ -1,11 +1,15 @@
 package com.pengxy.demo.framewrok;
 
+
+
+import com.sun.jndi.toolkit.url.Uri;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,5 +26,22 @@ public class LearningDemo {
     InputStream inputStream = resource.getInputStream();
 
     public LearningDemo() throws IOException {
+    }
+
+    private void scanPackage(String packageName){
+        Uri uri = this.getClass().getClassLoader().getResource(replaceTo(packageName));
+        String file  = uri.getFile();
+        File f = new Fill(file);
+
+        String[] list = f.list();
+        for(String path : list){
+            File filePath = new File(file + path);
+            if(filePath.isDirectory()){
+                // 递归
+                scanPackage(packageName + '.' + path);
+            }else {
+                classNames.add(packageName + '.'+path);
+            }
+        }
     }
 }
