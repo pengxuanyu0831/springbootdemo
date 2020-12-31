c8a37d0987c3e975ff92286015fb7895e4f1612c
pengxy <pengxuanyus@gmail.com>
2020-12-31 22:38:47 +0800

    二分法查找demo 1.1

--- src/main/java/com/pengxy/demo/NotionDemo/Algorithm/recursionBinarySearch.java
+++ src/main/java/com/pengxy/demo/NotionDemo/Algorithm/recursionBinarySearch.java
@@ -10,6 +10,11 @@ package com.pengxy.demo.NotionDemo.Algorithm;
 public class recursionBinarySearch {
 
     public static int recursionBinarySearch(int key,int[] arry,int low,int high){
+        if(key < arry[low] || key > arry[high] || low > high){
+            return -1;
+        }
+
+
         int middle = (low + high) / 2;
         if(arry[middle] < key){
             return recursionBinarySearch(key,arry ,middle + 1 ,high);
@@ -23,13 +28,13 @@ public class recursionBinarySearch {
 
     public static void main(String[] args) {
         int[] arry = {1,2,3,4,5,6,7,8};
-        int key = 3;
+        int key = 1;
 
         int find = recursionBinarySearch(key,arry,0,arry.length -1);
-        if(find ==-1){
-            System.out.println("find = 4");
+        if(find == -1){
+            System.out.println("找的是" + key + ", 位置是 " + find);
         }else {
-            System.out.println("find != 4");
+            System.out.println("找的是" + key + ", 位置是" + find);
         }
     }
 
